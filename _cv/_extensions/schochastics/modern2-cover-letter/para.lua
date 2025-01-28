d1a326e79cc705208871f167b79865f5dae1471d
Erik Fasterius <erik.fasterius@outlook.com>
2025-01-28 11:47:01 +0100

    Remove cover-letter-related files

--- _cv/_extensions/schochastics/modern2-cover-letter/para.lua
+++
@@ -1,5 +0,0 @@
-Para = function(para)
-    table.insert(para.content, 1, pandoc.RawInline('tex', ""))
-    table.insert(para.content, pandoc.RawInline('tex', "\\\\[4pt]"))
-    return para
-end
--- _cv/_extensions/schochastics/modern2-cover-letter/styles.lua
+++
@@ -1,4 +0,0 @@
-function Header(el)
-    local cv = pandoc.RawInline('latex', '\\cvsection{' .. pandoc.utils.stringify(el.content) .. '} \\vspace{4pt}')
-    return cv
-end
